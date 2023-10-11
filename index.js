f03fcdd8694011f05d916182c6acc6ed88d1c52f
Angelo Mateus <angelomateusnerysouza@gmail.com>
2023-10-11 17:27:20 -0300

    First Commit

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const app = require('express')();
 
-app.listen(8080, () => {
-    console.log('servidor rodando')
-})
 
-app.get('/gasolina', (req, res) => {
+app.get('/', (req, res) => {
     res.json(
         [
             {
-                posto: 'Posto Aldo',
-                rua: 'Rodovia dos Imigrantes, Km 05',
-                bairro: 'Distrito Industrial',
-                cidade: 'Cuiabá - MT',
-                gasolina: 5.40
+                "nome" : "Posto Aldo",
+                "rua" : "Rodovia dos Imigrantes, Km 05",
+                "bairro" : "Distrito Industrial",
+                "cidade" : "Cuiabá - MT",
+                "gasolina" : 5.45
             },
-
+        
             {
-                posto: 'Posto XV',
-                rua: 'Avenida Xv de Novembro, 30',
-                bairro: 'Porto',
-                cidade: 'Cuiabá - MT',
-                gasolina: 5.43
+                "nome" : "Posto XV",
+                "rua" : "Avenida Xv de Novembro, 30",
+                "bairro" : "Porto",
+                "cidade" : "Cuiabá - MT",
+                "gasolina" : 5.43
             },
-
+        
             {
-                posto: 'Posto Redserv',
-                rua: 'Rua Professor Francisco Torres, 85',
-                bairro: 'Araés',
-                cidade: 'Cuiabá - MT',
-                gasolina: 5.45
-            },
-
+                "nome" : "Posto Emboava Miguel Sutil",
+                "rua" : "Avenida Miguel Sutil, 4708",
+                "bairro" : "Alvorada",
+                "cidade" : "Cuiabá - MT",
+                "gasolina" : 5.45
+            }
         ]
     )
 })
+
+
+app.listen(8080, (req, res) => {
+    console.log('rodando')
+})
+
+
+
